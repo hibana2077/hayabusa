234396fbe943667f487b3ea186dd278ca2c0ed46
hibana2077 <hibana2077@gmail.com>
2024-01-02 23:05:32 +0800

    Delete unused files and update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,54 @@
 '''
 Author: hibana2077 hibana2077@gmail.com
-Date: 2023-10-14 12:20:25
+Date: 2024-01-02 21:43:38
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-02 18:40:59
-FilePath: \haya\src\main.py
+LastEditTime: 2024-01-02 23:05:27
+FilePath: \hayabusa\src\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+from operator import index
 import streamlit as st
+import plotly.express as px
+from pycaret.regression import setup, compare_models, pull, save_model, load_model
+import pandas_profiling
+import pandas as pd
+from streamlit_pandas_profiling import st_profile_report
+import os 
 
-if __name__ == '__main__':
-        
-    st.title('Welcome to Hayabusa')
+if os.path.exists('./dataset.csv'): 
+    df = pd.read_csv('dataset.csv', index_col=None)
+
+with st.sidebar: 
+    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
+    st.title("AutoNickML")
+    choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
+    st.info("This project application helps you build and explore your data.")
+
+if choice == "Upload":
+    st.title("Upload Your Dataset")
+    file = st.file_uploader("Upload Your Dataset")
+    if file:
+        df = pd.read_csv(file, index_col=None)
+        df.drop("Unnamed: 0", axis=1, inplace=True)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == "Profiling": 
+    st.title("Exploratory Data Analysis")
+    profile_df = df.profile_report()
+    st_profile_report(profile_df)
+
+if choice == "Modelling": 
+    chosen_target = st.selectbox('Choose the Target Column', df.columns)
+    if st.button('Run Modelling'): 
+        setup(df, target=chosen_target)
+        setup_df = pull()
+        st.dataframe(setup_df)
+        best_model = compare_models()
+        compare_df = pull()
+        st.dataframe(compare_df)
+        save_model(best_model, 'best_model')
+
+if choice == "Download": 
+    with open('best_model.pkl', 'rb') as f: 
+        st.download_button('Download Model', f, file_name="best_model.pkl")
