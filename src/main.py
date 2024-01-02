4363beb9843dae4f088f4239e91e4b49e8bbfb27
hibana2077 <hibana2077@gmail.com>
2024-01-02 23:37:31 +0800

    Update LastEditTime and sidebar title in main.py

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-01-02 21:43:38
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-02 23:34:33
+LastEditTime: 2024-01-02 23:37:08
 FilePath: \hayabusa\src\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -21,9 +21,8 @@ if os.path.exists('./dataset/') == False:
 if os.path.exists('./model/') == False:
     os.mkdir('./model/')
 
-with st.sidebar: 
-    st.image("https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png")
-    st.title("AutoNickML")
+with st.sidebar:
+    st.title("AutoML")
     choice = st.radio("Navigation", ["Upload","Profiling","Modelling", "Download"])
     st.info("This project application helps you build and explore your data.")
 
