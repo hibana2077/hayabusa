abd8959ba30102e5ff644b5a358f1a2230d82176
hibana2077 <hibana2077@gmail.com>
2024-01-02 23:05:58 +0800

    Remove unused import statement

--- src/main.py
+++ src/main.py
@@ -10,7 +10,6 @@ from operator import index
 import streamlit as st
 import plotly.express as px
 from pycaret.regression import setup, compare_models, pull, save_model, load_model
-import pandas_profiling
 import pandas as pd
 from streamlit_pandas_profiling import st_profile_report
 import os 
