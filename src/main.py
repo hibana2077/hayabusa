7706576eac723a14f34ba6168e5f0d2f70ec7f9f
hibana2077 <hibana2077@gmail.com>
2024-01-05 22:13:17 +0800

    Update ML model setup with train_size parameter

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-01-02 21:43:38
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-04 22:48:53
+LastEditTime: 2024-01-05 22:13:09
 FilePath: \hayabusa\src\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -49,11 +49,12 @@ if choice == "Modelling":
     df:pd.DataFrame = st.session_state['df']
     chosen_target = st.selectbox('Choose the Target Column', df.columns)
     drop_columns = st.multiselect('Choose the Columns to Drop', df.columns)
+    train_size = st.slider('Choose the Train Size', 0.1, 0.9, 0.7)
     ml_task = st.selectbox('Choose the ML Task', ['Classification', 'Regression'])
     if st.button('Run Modelling'):
         if ml_task == 'Classification':
             from pycaret.classification import setup, compare_models, pull, save_model, get_config
-            setup(df, target=chosen_target, ignore_features=drop_columns)
+            setup(df, target=chosen_target, ignore_features=drop_columns, train_size=train_size)
             setup_df = pull()
             st.dataframe(setup_df)
             best_model = compare_models(exclude=['lightgbm'])
@@ -68,7 +69,7 @@ if choice == "Modelling":
                 pickle.dump(pipeline, f)
         else:
             from pycaret.regression import setup, compare_models, pull, save_model
-            setup(df, target=chosen_target, ignore_features=drop_columns)
+            setup(df, target=chosen_target, ignore_features=drop_columns, train_size=train_size)
             setup_df = pull()
             st.dataframe(setup_df)
             best_model = compare_models(exclude=['lightgbm'])
