7c15e46f53afbbdb835f0b77359027f999042aa6
hibana2077 <hibana2077@gmail.com>
2024-01-14 14:14:35 +0800

    Update LastEditors and LastEditTime in main.py

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-01-02 21:43:38
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-01-05 22:13:09
+LastEditTime: 2024-01-14 14:14:35
 FilePath: \hayabusa\src\main.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+Description: main page
 '''
 from operator import index
 import streamlit as st
